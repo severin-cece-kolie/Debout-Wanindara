a350447e0ddbe3a01a008e843dd5533eaeacc11f
Optimus45s <db064446@gmail.com>
2025-11-21 00:09:02 +0100

    settings selection language

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.utils import translation
+from django.urls import translate_url
 
 
 def about(request):
     return render(request, 'pages/about.html')
+
